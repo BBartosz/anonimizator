c3ae7828fa429ae37902ba5689ee874e611c12c6
BBartosz <bbartek91@gmail.com>
2014-03-25 17:38:31 +0100

    not sure what wrong in anonimize string

--- anonimizator.rb
+++ anonimizator.rb
@@ -22,26 +22,31 @@ class Anonimizator
   def anonimize_records(table, columns_array)
     table.all.each do |record|
       columns_array.each do |column|
-        if column == 'email'
-          record_to_replace = anonimize_email(record[column])
-          ecord.update_attribute(column, record_to_replace)
-        else
-          record_to_replace = anonimize_string(record[column])
-          record.update_attribute(column, record_to_replace) 
-        end
+        anonimize_column(record, column)
       end
     end
   end
 
-  def anonimize_email(emailstring)
-    splitted_email_array = emailstring.split('@')
-    first_part_of_email  = splitted_email_array[0]
-    splitted_email_array[0]  = first_part_of_email[0] + '-' * (first_part_of_email.length - 2) + first_part_of_email[-1]
-    splitted_email_array.join('@')
+  def anonimize_column (record, column)
+    if column.include? ('@')
+      record_to_replace = anonimize_email(record[column])
+      record.update_attribute(column, record_to_replace)
+    else
+      record_to_replace = record[column] 
+      record_to_replace[1..-2] = anonimize_string(record[column])
+      record.update_attribute(column, record_to_replace) 
+    end
+  end
+
+  def anonimize_email(email)
+    parts    = email.split('@')
+    parts[0][1..-2] = anonimize_string(parts[0])
+    parts.join('@')
   end
 
   def anonimize_string(string)
-    string[0] + '-' * (string.length - 2) + string[-1]
+    return string if string.length < 3
+    string[1..-2] = '-' * (string.length - 2)
   end
 end
 
@@ -50,6 +55,6 @@ end
 anonim = Anonimizator.new
 anonim.connect_to_db('database.yml')
 
-anonim.select_tables({:offer => ['city'], :user => ['email']})
-backup = Backup.new('database.yml')
-backup.create_backup
+anonim.select_tables({:offer => ["city", "property_form"], :user => ['email']})
+# backup = Backup.new('database.yml')
+# backup.create_backup
